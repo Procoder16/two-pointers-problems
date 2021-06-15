d925d61f4a7a8f80cc786e9c2ad62b135b96e65f
Procoder16 <smsoumik1608@gmail.com>
2021-06-16 00:55:11 +0530

    documentation added

--- longestSubstringWithoutRepeatingCharacters.cpp
+++ longestSubstringWithoutRepeatingCharacters.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 /* 
-  
+    The idea behind the approach is that:
+    1. we will create a vector of char for 256 elements(no. of characters in any programming language) and initialise with -1
+    2. we will traverse the string and for each element we will update that element's index in the dict vector with the index in the string
+    3. if a char is found repeating, greater than -1, or greater than that tracking variable, we will update that variable's value with
+    the new index
 */
 
 int main(){
