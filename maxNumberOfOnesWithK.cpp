b5b9cb77ac5d4c8d7819c8d7ba90190d6cced293
Procoder16 <smsoumik1608@gmail.com>
2021-06-15 13:39:13 +0530

    documentation added

--- maxNumberOfOnesWithK.cpp
+++ maxNumberOfOnesWithK.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 /* 
-
+    The idea of solving the problem is as follows:
+    1. initially we will fix i at 0 and j at zero too
+    2. we will traverse the vector with j and if encountered with zero, will increment the zero count
+    3. now if zero count becomes greated than k, we will shrink the array from the right by i++
+    4. keep calculating the max length of subarray in each iteration
 */
 
 int main(){
